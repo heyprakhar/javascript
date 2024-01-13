5cad0eafbd3a39da3668461536c999344598db9b
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-13 13:03:43 +0000

    update 03_ConversionOperations

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -1,12 +1,32 @@
 let score = "33"
 let num = "33abc"
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(typeof(valueInNumber));
-console.log(typeof num);
-console.log(num);
+//console.log(typeof valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(typeof num);
+// console.log(num);
 //"33" ==> gets converted in string on putting between inverted commas 
 //"33abc" ==> doesn't gets converted in string even on putting between inverted commas and gives NaN(not a number as the o/p).
+
+//*******Operations in JS*******//
+
+let value = 5
+let negValue = - value // negative of the value of the number stored in value
+//console.log(negValue);
+// console.log(2**3) // 2 raised to power 3
+// console.log(4**3);
+// console.log(5%2);
+let str1 = "Hello"
+let str2 = " Prakhar"
+let str3 = str1 + str2
+//console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(2+2+"2");
+// console.log("2"+2+2);
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
