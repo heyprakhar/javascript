93f105924329cacfc20c99e663516e3a982ac604
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-15 16:41:32 +0000

    update datatypes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,1 +1,15 @@
 // # primitive and non-primitive datatypes - differentiated on the basis of call by value and call by reference
+
+// primitive - 7 categories
+// 1. String
+// 2. number
+// 3. boolean
+// 4. null
+// 5. undefined 
+// 6.symbol - used to make a value unique (used in adv. javascript) 
+// 7. BigInt - used for big values 
+
+// JAVASCRIPT is a dynamically typed language, which means that data types of variables are determined by the value they hold at runtime and can change throughout the program as we assign different values to them.
+
+//REFERENCE TYPE (non primitive)-
+// array,objects,functions  
