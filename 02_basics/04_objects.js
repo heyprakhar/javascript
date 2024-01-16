5b98fd9cbbff1130b22f27f375ccfe9a4ead73f6
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-16 19:33:36 +0000

    update objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,31 @@
+//const tinderUser = new Object() //singleton object
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "Prakhar@google.com",
+    fullname : {
+        userfullname: {  
+            firstname: "prakhar",
+            lastname :"tripathi",       //nesting of objects
+
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.lastname);
+
+
+// merging of values
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"c",4:"d"}
+const obj3 = {5:"e",6:"f"}
+// first method to merge the objects 
+//const obj4 = Object.assign({}, obj1 , obj2,obj3) // {} - empty parentheses acts as target and signifies the other objects as source as syntax of assign is {target,source} , it is not necessary to add an empty parentheses but it is a good practice to do.
+
+// 2nd method to merge the objects
+obj4 ={...obj1,...obj2,...obj3} //spread method - easiest and the most advanced method to merge the objects
+console.log(obj4); 
