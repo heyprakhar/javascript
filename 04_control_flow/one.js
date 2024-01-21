8c04bd0a694b8734cb80efcfe394a196d6b795e6
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-21 07:44:30 +0000

    control flow one

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,4 +1,26 @@
-const score = 200
-if (score) {
-    
+// const score = 200
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// }
+
+// use of nesting in actual products - 
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard) {
+    console.log("allow to purchase course");
+} else {
+    console.log("do not allow to purchase course");
+}
+
+// another example - 
+const loggedInfromGmail = true
+const loggedInfromGoogle =false 
+
+if (loggedInfromGmail || loggedInfromGoogle) {
+    console.log("user allowed"); // || stands for "or" and if any of the condition is true then it allows to enter the code block
+} else {
+    console.log("user not allowed");
 }
