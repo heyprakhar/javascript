cb79eb3f3aae5bf8962021beffb30ac11bc0c34b
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-21 09:47:31 +0000

    update control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,20 @@
+const month = 4
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2 :
+        console.log("feb");
+        break;    
+    case 3:
+        console.log("march");
+        break;
+    case 4 :
+        console.log("april");
+        break;    
+
+    default:
+        console.log("default case match");
+        break;
+}
---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,7 @@
+// truthy and falsy values - always true and always false 
+
+// falsy values - 
+// false , 0 , -0, BigInt = 0n,"", null , undefined , NaN (these all are truthy values)
+
+// truthy values - "0" , 'false' , " "(a space inside an string),[],{}, function(){} (an empty function is also a truthy value)
+
