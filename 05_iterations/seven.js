1827982df746f2fc6b4b0f3bb5019b8a3c0ff21d
Prakhar Tripathi <115215167+heyprakhar@users.noreply.github.com>
2024-01-23 14:10:11 +0000

    map method

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,1 +1,14 @@
-const myNums = []
+const myNumbers = [11,12,13,14,15,16,17,18,19,20] 
+
+// const numbers = myNumbers.map( (num)=>{
+//  return num + 10 
+// })
+// console.log(numbers);
+
+// chaining of functions - in this technique , we use multiple types of function in multiple times 
+
+const newNum = myNumbers
+.map((num)=> num*10)
+.map((num)=> num + 1)
+
+console.log(newNum);
